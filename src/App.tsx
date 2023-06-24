21f4495e9ae867f29b2cce582422cd19848355ae
Nazidev01 <toktonalykyzynazi3675478@gmail.com>
2023-06-24 14:58:03 +0600

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
+import Info from "./Pages/Info/Info";
+
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Hero/>
+        <Footer/>
+      {/*<Info/>*/}
     </div>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,51 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div id="footer">
+            <div className="container">
+                <div className="footer">
+                    <div className="footer-block">
+                        <h1>Sign up for our newsletter</h1>
+                        <p>Be the first to know about our special offers, news, and updates.</p>
+                        <div className="btn">
+                            <input type="text" placeholder="Email Address"/>
+                            <button>Sign Up</button>
+                        </div>
+                    </div>
+                    <div className="footer-block">
+                        <h3>Lorem Ipsum</h3>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                    </div>
+                    <div className="footer-block">
+                        <h3>Lorem Ipsum</h3>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                    </div>
+                    <div className="footer-block">
+                        <h3>Lorem Ipsum</h3>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                    </div>
+                    <div className="footer-block">
+                        <h3>Lorem Ipsum</h3>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                        <h6>Lorem</h6>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
