1829aa36e8273b24a105f9e9d579c8ec0d796692
asadulladev <usmanbeliever@gmail.com>
2023-07-01 20:34:14 +0600

    thunk

--- src/Pages/Global/Global.tsx
+++ src/Pages/Global/Global.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
-import BookList from "../Tabs/UserItems";
+import BookList from "../Tabs/Tabs";
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
